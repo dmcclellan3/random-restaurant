b7c4166edcde4bd6da3b5cabbdcd6597950f5e89
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-24 11:58:07 -0400

    saving

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,41 @@
-function Header() {
-  return (
-    <div>
-      Menu
-    </div>
-  )
-}
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+    
+    function Header() {
+      return (
+        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+          <Container>
+              <Col xs={4} md={2}>
+              <Image id="header-logo" src="https://cdn.shopify.com/s/files/1/0050/0518/1021/collections/Dad_Bod_Decals_1200x1200.png?v=1590420252" thumbnail />
+              </Col>
+              
+            <Navbar.Brand>Dad Bod's Bar & Grill</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#features">Menu</Nav.Link>
+                <Nav.Link href="#pricing"></Nav.Link>
+                <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Contact Us</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Location
+                  </NavDropdown.Item>
+                  {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
+                </NavDropdown>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#deets">More deets</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+    }
 
-export default Header
+export default Header;
