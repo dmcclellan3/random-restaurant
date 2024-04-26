a7df2b93c6a5eb540285c882c9002ecd164607da
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-26 10:06:26 -0400

    adding comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom"
 import { useEffect, useState } from "react";
 
-
+//Title rendering
 const Title = ({section}) => {
   return (
     <h2 className="d-flex justify-content-center mt-5 mb-4">
@@ -9,7 +8,7 @@ const Title = ({section}) => {
     </h2>
   )
 }
-
+//Component for rendering food items/details
 function Food({item}){
   return(
     <div key={item.id}>
@@ -20,17 +19,17 @@ function Food({item}){
     </div>
   )  
 }
-
+//Main app
 function App() {
-  const [menuItems, setMenuItems] = useState([])
+  const [menuItems, setMenuItems] = useState([]) //Effect hook to fetch menu items when the component forms
   console.log(menuItems)
   useEffect(() => {
-    async function getMenu() {
+    async function getMenu() { //fetching items from API 
       const response = await fetch('https://www.jsonkeeper.com/b/MDXW');
       const allMenuItemsArray = await response.json();
       console.log('Menu Items Return: ', allMenuItemsArray)
       allMenuItemsArray.map(item =>  <Food item={item}/>)
-      setMenuItems(allMenuItemsArray)
+      setMenuItems(allMenuItemsArray) //Setting the menu items to state variable 
       
     }
     getMenu()
@@ -38,13 +37,13 @@ function App() {
     
   return (
     <div className="p-5">
-      {/* <Link to='/about'>About</Link> */}
       <Title
       section = {'Appetizers'} />
       <div id='menu-layout'>
-        <div id="appetizer-layout">
+          <div id="appetizer-layout">
           {menuItems.filter(menuItem =>  menuItem.category === 'Appetizer' || menuItem.cuisine_type === "American" && menuItem.cuisine_type === "Mexican").map(item =>  <Food item={item}/>)}
-
+          {/* || Logical OR/ && Logical AND/ OR returns true if at least one operand returns true   */}
+          {/* Logical AND returns true if both operands are true */}
         </div>
         
           <Title section = {'Lunch'} />
