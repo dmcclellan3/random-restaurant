c013663e3df666488358e196c67761cff90bafae
dmcclellan3 <wdmcclellan3@gmail.com>
2024-05-23 15:32:32 -0400

    menu displaying

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const Title = ({section}) => {
 function Food({item}){
   return(
     <div key={item.id}>
-      <h3>{item.title}</h3>
+      <h3>{item.name}</h3>
       <p><em>{item.description}</em></p>
       <p><strong>${item.price}</strong></p>
       <div id="item-borders"></div>
@@ -23,40 +23,51 @@ function Food({item}){
 //Main app
 function App() {
   const [menuItems, setMenuItems] = useState([]) //Effect hook to fetch menu items when the component forms
+  const [appetizers, setAppetizers] = useState([])
   console.log(menuItems)
   useEffect(() => {
     async function getMenu() { //fetching items from API 
-      const response = await fetch('https://www.jsonkeeper.com/b/MDXW');
+      const response = await fetch('http://127.0.0.1:8000/menuitems/');
       const allMenuItemsArray = await response.json();
-      console.log('Menu Items Return: ', allMenuItemsArray)
-      allMenuItemsArray.map(item =>  <Food item={item}/>)
-      setMenuItems(allMenuItemsArray) //Setting the menu items to state variable 
-      
+      setMenuItems(allMenuItemsArray)
+      // foodComponents()
+      // const allAppetizersArray = allMenuItemsArray.filter(menuItem => menuItem.category_name === 'Appetizer')
+      // console.log('Menu Items Return: ', allMenuItemsArray)
+      // const anArrayOfFoodComponents = allMenuItemsArray.map(item =>  <Food key={item.id} item={item}/>)
+      // setMenuItems(anArrayOfFoodComponents) //Setting the menu items to state variable 
+      // const appetizerComponents = allAppetizersArray.map(menuItem => <Food key={}/>)
     }
     getMenu()
   }, [])
+
+
+  const FoodComponents = ({categoryName}) => {
+    const foodArray = menuItems.filter(item => item.category_name === categoryName);
+    console.log(foodArray)
+    return foodArray.map(item => <Food item={item} key={item.id}/>)
+  };  
+
     
+  // {menuItems.filter(menuItem =>  menuItem.category_name === 'Appetizer')}
   return (
     <div className="p-5">
       <Title
       section = {'Appetizers'} />
-      <p classname='d-flex justify-content-right'><em>Ask your server about rotating drafts</em></p>
+      <FoodComponents categoryName={'Appetizers'}/>
+      {/* {menuItems} */}
+      <p className='d-flex justify-content-right'><em>Ask your server about rotating drafts</em></p>
       <div id='menu-layout'>
           <div id="appetizer-layout">
-          {menuItems.filter(menuItem =>  menuItem.category === 'Appetizer' || menuItem.cuisine_type === "American" && menuItem.cuisine_type === "Mexican").map(item =>  <Food item={item}/>)}
           {/* || Logical OR/ && Logical AND/ OR returns true if at least one operand returns true   */}
           {/* Logical AND returns true if both operands are true */}
         </div>
         
           <Title section = {'Lunch'} />
-          
-        <div id="lunch-layout">
-          {menuItems.filter(menuItem =>  menuItem.category === 'Lunch' || menuItem.cuisine_type === "American" && menuItem.cuisine_type === "Mexican").map(item =>  <Food item={item}/>)}
-        </div>
+          <FoodComponents categoryName={'Lunch'}/>
+      
           <Title section = {'Dinner'} />
-        <div id="dinner-layout">
-          {menuItems.filter(menuItem =>  menuItem.category === 'Dinner' || menuItem.cuisine_type === "American" && menuItem.cuisine_type === "Mexican").map(item =>  <Food item={item}/>)}
-        </div>
+          <FoodComponents categoryName={'Dinner'}/>
+          
       </div>
     </div>
   )
